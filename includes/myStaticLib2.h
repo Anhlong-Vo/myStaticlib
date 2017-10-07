dea19a6c7a026a14504e41228aaaf332de894a76
Keith <kperkins411@gmail.com>
2017-10-06 23:02:21 -0400

    goes with myStaticLibTest project

---
+++ includes/myStaticLib2.h
@@ -0,0 +1,13 @@
+/*
+ * myStaticLib2.h
+ *
+ *  Created on: Oct 6, 2017
+ *      Author: keith
+ */
+
+#ifndef MYSTATICLIB2_H_
+#define MYSTATICLIB2_H_
+
+void show();
+
+#endif /* MYSTATICLIB2_H_ */
---
+++ myStaticLib2.cpp
@@ -0,0 +1,14 @@
+/*
+ * myStaticLib2.cpp
+ *
+ *  Created on: Oct 6, 2017
+ *      Author: keith
+ */
+#include "./includes/myStaticLib2.h"
+
+#include <iostream>
+
+void show(){
+
+	std::cout<<"In library"<<std::endl;
+}
